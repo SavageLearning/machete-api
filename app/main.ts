468f9e87c9dca517e08c71d7a75d75435e890d64
Jimmy Carter <jciispam@gmail.com>
2016-11-19 11:23:28 -0800

    Added repository namespace

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import * as Knex from 'knex';
 import * as Bookshelf from 'bookshelf';
 
+namespace Repository {
 var config = {
     user: process.env.MCHT_DB_USER,
     password: process.env.MCHT_DB_PASSWORD,
@@ -15,25 +16,26 @@ var knex = Knex({client: 'mssql', connection: config });
 var bookshelf = Bookshelf(knex);
 
 
-class Employers extends bookshelf.Model<Employers> {
+export class Employers extends bookshelf.Model<Employers> {
     get tableName() { return 'employers'; }
     workOrders(): Bookshelf.Collection<WorkOrders> {
         return this.hasMany(WorkOrders);
     }
 }
 
-class WorkOrders extends bookshelf.Model<WorkOrders> {
+export class WorkOrders extends bookshelf.Model<WorkOrders> {
     get tableName() { return 'workorders'; }
     workAssignments(): Bookshelf.Collection<WorkAssignments> {
         return this.hasMany(WorkAssignments);
     }
 }
 
-class WorkAssignments extends bookshelf.Model<WorkAssignments> {
+export class WorkAssignments extends bookshelf.Model<WorkAssignments> {
     get tableName() { return 'workassignments'; }
 }
+}
 
-new Employers().where('ID', 1).fetch().then(function(employer) {
+new Repository.Employers().where('ID', 1).fetch().then(function(employer) {
     console.log(employer.toJSON());
 }).catch(function(err) {
     console.error(err);
--- dist/main.js
+++ dist/main.js
@@ -6,60 +6,66 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var Knex = require('knex');
 var Bookshelf = require('bookshelf');
-var config = {
-    user: process.env.MCHT_DB_USER,
-    password: process.env.MCHT_DB_PASSWORD,
-    server: process.env.MCHT_DB_SERVER,
-    database: process.env.MCHT_DB,
-    options: {
-        encrypt: true // Use this if you're on Windows Azure
-    }
-};
-var knex = Knex({ client: 'mssql', connection: config });
-var bookshelf = Bookshelf(knex);
-var Employers = (function (_super) {
-    __extends(Employers, _super);
-    function Employers() {
-        _super.apply(this, arguments);
-    }
-    Object.defineProperty(Employers.prototype, "tableName", {
-        get: function () { return 'employers'; },
-        enumerable: true,
-        configurable: true
-    });
-    Employers.prototype.workOrders = function () {
-        return this.hasMany(WorkOrders);
+var Repository;
+(function (Repository) {
+    var config = {
+        user: process.env.MCHT_DB_USER,
+        password: process.env.MCHT_DB_PASSWORD,
+        server: process.env.MCHT_DB_SERVER,
+        database: process.env.MCHT_DB,
+        options: {
+            encrypt: true // Use this if you're on Windows Azure
+        }
     };
-    return Employers;
-}(bookshelf.Model));
-var WorkOrders = (function (_super) {
-    __extends(WorkOrders, _super);
-    function WorkOrders() {
-        _super.apply(this, arguments);
-    }
-    Object.defineProperty(WorkOrders.prototype, "tableName", {
-        get: function () { return 'workorders'; },
-        enumerable: true,
-        configurable: true
-    });
-    WorkOrders.prototype.workAssignments = function () {
-        return this.hasMany(WorkAssignments);
-    };
-    return WorkOrders;
-}(bookshelf.Model));
-var WorkAssignments = (function (_super) {
-    __extends(WorkAssignments, _super);
-    function WorkAssignments() {
-        _super.apply(this, arguments);
-    }
-    Object.defineProperty(WorkAssignments.prototype, "tableName", {
-        get: function () { return 'workassignments'; },
-        enumerable: true,
-        configurable: true
-    });
-    return WorkAssignments;
-}(bookshelf.Model));
-new Employers().where('ID', 1).fetch().then(function (employer) {
+    var knex = Knex({ client: 'mssql', connection: config });
+    var bookshelf = Bookshelf(knex);
+    var Employers = (function (_super) {
+        __extends(Employers, _super);
+        function Employers() {
+            _super.apply(this, arguments);
+        }
+        Object.defineProperty(Employers.prototype, "tableName", {
+            get: function () { return 'employers'; },
+            enumerable: true,
+            configurable: true
+        });
+        Employers.prototype.workOrders = function () {
+            return this.hasMany(WorkOrders);
+        };
+        return Employers;
+    }(bookshelf.Model));
+    Repository.Employers = Employers;
+    var WorkOrders = (function (_super) {
+        __extends(WorkOrders, _super);
+        function WorkOrders() {
+            _super.apply(this, arguments);
+        }
+        Object.defineProperty(WorkOrders.prototype, "tableName", {
+            get: function () { return 'workorders'; },
+            enumerable: true,
+            configurable: true
+        });
+        WorkOrders.prototype.workAssignments = function () {
+            return this.hasMany(WorkAssignments);
+        };
+        return WorkOrders;
+    }(bookshelf.Model));
+    Repository.WorkOrders = WorkOrders;
+    var WorkAssignments = (function (_super) {
+        __extends(WorkAssignments, _super);
+        function WorkAssignments() {
+            _super.apply(this, arguments);
+        }
+        Object.defineProperty(WorkAssignments.prototype, "tableName", {
+            get: function () { return 'workassignments'; },
+            enumerable: true,
+            configurable: true
+        });
+        return WorkAssignments;
+    }(bookshelf.Model));
+    Repository.WorkAssignments = WorkAssignments;
+})(Repository || (Repository = {}));
+new Repository.Employers().where('ID', 1).fetch().then(function (employer) {
     console.log(employer.toJSON());
 }).catch(function (err) {
     console.error(err);
